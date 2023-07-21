3492831db1cb9423c7e91d3903b32785eee6b35e
LazyDotExe <papaporo@proton.me>
2023-07-21 11:25:26 -0500

    I have no idea

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,88 +7,134 @@ function handleReady() {
 
 
   getGuess()
+  // getRandomNumber()
 
 }
+//-------------------------------------------------------------------------------------------
 let guessNumber;
 let timesGuessed = 0;
+let magicNbr;
 
-let submitBtn = () => {
-    const playerGuess = {
-      player1: $("#input1").val(),
-      player2: $("#input2").val()
-
-} // set up a class for the input's to clear both at once
-  $('.inputs').val('');
-    console.log(playerGuess)
-    // $('span').text('');
-    // timesGuessed += 1;
-    // $('span').append(`${timesGuessed}`)
+//-------------------------------------------------------------------------------------------
 
 
+let submitBtn = () => {  // This saves both players inputs as an object.
+  const playerGuess = {
+    player1: $("#input1").val(),
+    player2: $("#input2").val(),
+  }
 
+  $('.inputs').val('');  // This clears the value in the input fields
+  console.log(playerGuess)
+  $('span').text(''); //------------------
+  timesGuessed += 1; //--------------------This clears the span area and appends the times
+  $('span').append(`${timesGuessed}`) //-- Guessed variable in increments of 1
 
+  $.ajax({
+    method: "POST",  // this block will "POST" the playerGuess var to "/guessed" located server side.
+    url: "/guessed",
+    data: playerGuess
+  }).then((response) => {
+    console.log("POST was successful:", response)
 
     $.ajax({
-        method: "POST", 
-        url: "/guessed", 
-        data: playerGuess 
+      method: "POST",
+      url: "/getRandom",
+      data: playerGuess
     }).then((response) => {
-        console.log("POST was successful:", response)
+      console.log("POST was successful:", response)
 
-        getGuess()
-        render()
+
+
+      getGuess()
+      render()
 
     }).catch((error) => {
-        console.log("Error with POST request:", error)
-        alert("Error with POST")
+      console.log("Error with POST request:", error)
+      alert("Error with POST")
     })
+  
 
-
+  })
 }
+//-------------------------------------------------------------------------------------------
 
 
 let getGuess = () => {
-    
+    console.log("getGuess")
 
-  console.log("getGuess")
-
-  
-  $.ajax({
-      
+    $.ajax({
       method: 'GET',
       url: '/listOfGuesses'
-  }).then((response) => {
+    }).then((response) => {
+      console.log(response)
+      guessNumber = response
+
+      
+      render()
+
+    }).catch((error) => {
+      alert("Request Failed")
+      console.log("Request failed, error:", error)
+    })      
+  }
+  let getRandomNumber = () => {
+    console.log("getRandomNumber");
+  
+    $.ajax({
+      method: 'GET',
+      url: '/getRandom'
+    }).then((response) => {
+      console.log('this is the random number', response);
+      magicNbr = response
+      render()
+
+      // Do something with the random number response
+    }).catch((error) => {
+      alert("Request Failed");
+      console.log("Request failed, error:", error);
+    });
+    
+    
+  };
+
+//-------------------------------------------------------------------------------------------
+let getRandom = () => {
+    console.log("getRandom")
+
+    $.ajax({
+      method: 'GET',
+      url: '/getRandom'
+    }).then((response) => {
       console.log(response)
       guessNumber = response
       render()
-  }).catch((error) => {
+
+    }).catch((error) => {
       alert("Request Failed")
       console.log("Request failed, error:", error)
-  })
-}
+    })
+  }
+
+
+//-------------------------------------------------------------------------------------------
+
 
 let render = () => {
-  $('#tableInfo').empty()
+    $('#tableInfo').empty()
 
-  for (let guess of guessNumber) {
+    for (let guess of guessNumber) {
       console.log(guess)
       $('#tableInfo').append(`
       <tr>
           <td class="bold">${guess.player1}:</td>
+          <td class="bold">${magicNbr}:</td>
           <td class="bigText">${guess.player2}</td>
       </tr>`)
-    //   $('span').text('');
-    
-    // $('span').append(`${timesGuessed}`)
+
+    }
+
   }
-  
-}
 
 
-let resetBtn = () => console.log('inside reset')
-
-
-
-
-
-
+// let resetBtn = () => console.log('inside reset')
--- server/server.js
+++ server/server.js
@@ -16,47 +16,72 @@ app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
   
 })
+//-------------------------------------------------------------------------------------------
 
-// function randomNumber(num1, num2) {
-//   return Math.floor(Math.random() * (num2 - num1) + num1);
-    
-// }
 
-// console.log(randomNumber(1, 25))
-
-let timesGuessed;
-
+let randomNbr = Math.floor(Math.random() * (25 - 1) + 1);
+console.log(randomNbr);
 let guesses = [];
 
-app.post("/randomNbr",(req, res) => {
-    console.log("in randomNbr");
 
-    // let addItem = req.body
-    // inventory.push(addItem)
+//-------------------------------------------------------------------------------------------
 
-    // console.log("currentInventory:", inventory)
-    // res.send(inventory)
-    // res.sendStatus(201)
-})
 
 app.get('/listOfGuesses', (req, res) => {
   console.log("Arrived at /Guesses", guesses)
   
-  // res.send(timesGuessed)
   res.send(guesses)
 })
+
+//-------------------------------------------------------------------------------------------
+
 
 
 app.post("/guessed",(req, res) => {
-  console.log("Body for addquote:", req.body);
+  console.log("Body for guess:", req.body);
 
   let addGuess = req.body
   guesses.push(addGuess)
-
+ 
   console.log("Guesses:", guesses)
   res.send(guesses)
-  res.sendStatus(201)
+  // res.sendStatus(201)
 })
 
+//-------------------------------------------------------------------------------------------
 
-// timesGuessed += 1;
+
+app.get('/getRandom', (req, res) => {
+
+  res.send(randomNbr.toString());
+});
+
+
+
+//-------------------------------------------------------------------------------------------
+
+app.post('/getRandom', (req, res) => {
+  const mostRecentGuess = guesses[guesses.length - 1];
+  let toLow = "player1 is to low"
+  let toHigh = "player1 is to high"
+  let justRight = "player1 Wins"
+  let hint;
+  
+  if (mostRecentGuess.player1 < randomNbr) {
+    hint = toLow
+
+  } else if (mostRecentGuess.player1 > randomNbr) {
+     hint = toHigh
+
+  } else {
+    hint = justRight
+
+  }
+  res.send(hint)
+  
+
+});
+
+
+
+
